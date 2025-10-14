f112e72004567cc183416270d2468b56233fa187
CodexCommunion <codexcommunion@gmail.com>
2025-10-14 11:00:32 -0400

    adds protocol to url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: "https://codexcommunion.github.io",
-  url: "printpraypeel.com",
+  url: "https://printpraypeel.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: "/printpraypeel.com/",
